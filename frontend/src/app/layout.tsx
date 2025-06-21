4efc625c09a88b9b5f1d95a166190d72654e4b4c
hudy@mac-air <hudy9x@gmail.com>
2025-06-21 17:00:17 +0700

    create github star button

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner"
+import { GithubStar } from "@/components/GithubStar";
 
 import "./globals.css";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GithubStar />
         {children}
         <Toaster />
       </body>
