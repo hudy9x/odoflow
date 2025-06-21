2b4f2f38825e0ad0467315a8109adb7a8f4f745b
hudy@mac-air <hudy9x@gmail.com>
2025-06-21 14:04:43 +0700

    refactor: add a logo with app name to /login and /register pages

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,16 +1,14 @@
 import { LoginForm } from "@/features/Login"
+import Link from "next/link"
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <img src="/logo/app-icon-smile.png" alt="Logo" />
-            </div>
-            Odoflow
-          </a>
+          <Link href="/">
+            <img src="/logo/logo-with-name2.png" alt="Logo" width={90} />
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
